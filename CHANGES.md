a2f07babcd8eac19004ce0883601eed1bd6a4649
Karlis Lauva <k@karlis.me>
2016-01-19 19:17:46 -0700

    full.core.config: remove deprecations
    * remove defconfig, defoptconfig, and defmappedconfig in favor of
    more generic opt method.
    * remove deprecated FOX_CONFIG env variable for config path

---
+++ CHANGES.md
@@ -0,0 +1,5 @@
+# Breaking changes in 0.10
+
+* full.core
+  * removed config loader macros `defconfig`, `defoptconfig`, and `defmappedconfig`
+  * only `$FULL_CONFIG` is being checked for config file path
--- full.core/src/full/core/config.clj
+++ full.core/src/full/core/config.clj
@@ -2,31 +2,33 @@
   (:require [full.core.sugar :refer :all]
             [clojure.tools.cli :refer [parse-opts]]
             [clojure.java.io :refer [as-file]]
-            [clj-yaml.core :as yaml])
-  (:gen-class))
+            [clj-yaml.core :as yaml]))
+
 
 (defonce _config (atom {}))
 
 (def config-cli-options
   [["-c" "--config name" "Config filename"]])
 
-(defn config-file []
-  (let [f (-> ; allow to override via  command line
-            (:config (:options (parse-opts *command-line-args* config-cli-options)))
-            ; or env variable
-            (or (System/getenv "FULL_CONFIG"))
-            ; legacy fallback - deprecated
-            (or (System/getenv "FOX_CONFIG"))
-            ; of use dev.yaml as default
-            (or "dev.yaml")
-            (as-file))]
-    (when (not (.exists f))
-      (println "full.core.config - EXITING! - Configuration file" (.getAbsolutePath f) "not found.")
+(defn config-file
+  "Loads config from a file. Path is taken either from command line -c flag,
+   FULL_CONFIG env variable, or the default `dev.yaml` is used."
+  []
+  (let [f (-> (parse-opts *command-line-args* config-cli-options)
+              (:options) :config
+              ; or env variable
+              (or (System/getenv "FULL_CONFIG"))
+              ; of use dev.yaml as default
+              (or "dev.yaml")
+              (as-file))]
+    (when-not (.exists f)
+      (println "full.core.config - EXITING! - Configuration file"
+               (.getAbsolutePath f) "not found.")
       (System/exit 1))
     (println "full.core.config - Using config file" (.getAbsolutePath f))
     f))
 
-(defn normalize-config [config]
+(defn- normalize-config [config]
   (map-map keyword config))
 
 (defn configure
@@ -41,47 +43,6 @@
                          ; else - already configured, return exisiting config
                          config)))))
 
-(defn get-config [config & ks]
-  (let [v (get-in config ks)]
-    (when (nil? v)
-      (throw (RuntimeException. (str "Property " ks " is not configured"))))
-    v))
-
-(defn config [k & ks]
-  (let [v (apply get-config (into [@_config k] ks))]
-    (if (map? v)
-      (partial get-config v)
-      v)))
-
-(defn config? [k & ks]
-  (get-in @_config (into [k] ks)))
-
-(defmacro defconfig
-  "DEPRECATED: Use 'opt' instead.
-  Define required configuration property, for example (defconfig some-prop :some-prop). Defined properties should be
-  dereferenced with @ when used, for example @some-prop. When property is not configured, dereferencing it will throw
-  an exception."
-  {:deprecated "0.5.7"}
-  [name & ks]
-  `(def ~name (delay (get-config @_config ~@ks))))
-
-(defmacro defoptconfig
-  "DEPRECATED: Use 'opt' instead.
-  Define optional configuration property, for example (defoptconfig some-prop :some-prop). Defined properties should be
-  dereferenced with @ when used, for example @some-prop. When property is not configured, will return nil."
-  {:deprecated "0.5.7"}
-  [name & ks]
-  `(def ~name (delay (config? ~@ks))))
-
-(defmacro defmappedconfig
-  "DEPRECATED: Use 'opt' instead.
-  Define a configuration property, for example (defmappedconfig some-prop f :some-prop).
-  On creation will be passed as an argument to f, (for example, to create a set).
-  Defined properties should be dereferenced with @ when used, for example @some-prop.
-  When property is not configured, will return nil."
-  {:deprecated "0.5.7"}
-  [name f & ks]
-  `(def ~name (delay (~f (config? ~@ks)))))
 
 ::undefined
 
